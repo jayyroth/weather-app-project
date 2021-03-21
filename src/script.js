0bbd7cce16c14da54faa6666db6165acefc5efb6
jayyroth <77689999+jayyroth@users.noreply.github.com>
2021-03-21 18:44:06 -0500

    Fixed search engine response
    API was not responding; fixed URL

--- src/script.js
+++ src/script.js
@@ -51,16 +51,14 @@ function formatDate(date) {
 
   function search(city) {
     let apiKey = "d77489ebd0ba8b6fa1d62c2519e08052";
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-    let units = "metric";
-    let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(getDefaultTemperature);
   }
   
   function handleSubmit(event) {
     event.preventDefault();
-    let citySearchResult = document.querySelector("#search-city-result");
+    let citySearchResult = document.querySelector("#search-city-input");
     search(citySearchResult.value);
   }
 
